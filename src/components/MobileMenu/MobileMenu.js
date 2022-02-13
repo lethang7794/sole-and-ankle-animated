73e8d4d1c29815d87c21381e5da6526f934088b6
Thang Le Quang <lethang7794@gmail.com>
2022-02-14 00:03:12 +0700

    Exercise 3: Modal enter animation

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -8,33 +8,51 @@ import { QUERIES, WEIGHTS } from '../../constants';
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
+import { keyframes } from 'styled-components';
+
+const BackdropFadeInTime = 200;
+
+const DrawerSlideInTime = 300;
+const DrawerSlideInDelay = 200;
+
+const DrawerFadeInTime = 600;
+const DrawerFadeInDelay = 400;
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
   return (
     <Overlay isOpen={isOpen} onDismiss={onDismiss}>
-      <Content aria-label="Menu">
+      <Content aria-label='Menu'>
         <CloseButton onClick={onDismiss}>
-          <Icon id="close" />
+          <Icon id='close' />
           <VisuallyHidden>Dismiss menu</VisuallyHidden>
         </CloseButton>
         <Filler />
         <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
+          <NavLink href='/sale'>Sale</NavLink>
+          <NavLink href='/new'>New&nbsp;Releases</NavLink>
+          <NavLink href='/men'>Men</NavLink>
+          <NavLink href='/women'>Women</NavLink>
+          <NavLink href='/kids'>Kids</NavLink>
+          <NavLink href='/collections'>Collections</NavLink>
         </Nav>
         <Footer>
-          <SubLink href="/terms">Terms and Conditions</SubLink>
-          <SubLink href="/privacy">Privacy Policy</SubLink>
-          <SubLink href="/contact">Contact Us</SubLink>
+          <SubLink href='/terms'>Terms and Conditions</SubLink>
+          <SubLink href='/privacy'>Privacy Policy</SubLink>
+          <SubLink href='/contact'>Contact Us</SubLink>
         </Footer>
       </Content>
     </Overlay>
   );
 };
+
+const BackdropFadeIn = keyframes`
+  from {
+    background: transparent;
+  }
+  to {
+    background: var(--color-backdrop);
+  }
+`;
 
 const Overlay = styled(DialogOverlay)`
   position: fixed;
@@ -42,9 +60,29 @@ const Overlay = styled(DialogOverlay)`
   left: 0;
   right: 0;
   bottom: 0;
-  background: var(--color-backdrop);
   display: flex;
   justify-content: flex-end;
+  animation: ${BackdropFadeIn} ${BackdropFadeInTime}ms;
+  animation-fill-mode: both;
+`;
+
+const DrawerSlideIn = keyframes`
+  from {
+    transform: translateX(100%);
+  }
+
+  to {
+    transform: translateX(0);
+  }
+`;
+
+const DrawerFadeIn = keyframes`
+  from {
+    opacity: 0
+  }
+  to {
+    opacity: 1;
+  }
 `;
 
 const Content = styled(DialogContent)`
@@ -54,6 +92,13 @@ const Content = styled(DialogContent)`
   padding: 24px 32px;
   display: flex;
   flex-direction: column;
+  animation: ${DrawerSlideIn} ${DrawerSlideInTime}ms ${DrawerSlideInDelay}ms;
+  animation-fill-mode: both;
+
+  & > * {
+    animation: ${DrawerFadeIn} ${DrawerFadeInTime}ms ${DrawerFadeInDelay}ms;
+    animation-fill-mode: both;
+  }
 `;
 
 const CloseButton = styled(UnstyledButton)`
